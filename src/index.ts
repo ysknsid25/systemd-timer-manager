864e7383acef58ebf31a7dfca4acf2e9aa88d5b0
ysknsid25 <kengo071225@gmail.com>
2026-02-03 19:27:30 +0900

    chore: add eslint and fix lint error
    Signed-off-by: ysknsid25 <kengo071225@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
 #!/usr/bin/env node
-import { defineCommand, runMain } from "citty";
-import init from "./commands/init";
+import { defineCommand, runMain } from 'citty';
+import init from './commands/init';
 
 const main = defineCommand({
-    meta: {
-        name: "stm",
-        version: "1.0.0",
-        description: "Systemd Timer Manager",
-    },
-    subCommands: {
-        init,
-    },
-    run() {
-        // Show help if no subcommand is provided
-    },
+  meta: {
+    name: 'stm',
+    version: '1.0.0',
+    description: 'Systemd Timer Manager',
+  },
+  subCommands: {
+    init,
+  },
+  run() {
+    // Show help if no subcommand is provided
+  },
 });
 
 runMain(main);
